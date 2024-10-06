fb06f833f0755652df50c9693e743234ca779e33
joshkim2441 <joshkim2441@gmail.com>
2024-10-06 08:44:00 +0300

    site updates

--- files/scripts/index.js
+++ files/scripts/index.js
@@ -2,7 +2,7 @@
 document.getElementById("datetimepicker").value = new Date().toISOString().slice(0, 16);
 function updateDate() {
     const selectedDate = document.getElementById("datetimepicker").value;
-    document.querySelector(".datetimepicker").textContent = selectedDate;
+    document.querySelector(".datetimepicker").innerHTML = selectedDate;
 }
 
 // Add event listener to the input element
@@ -89,12 +89,16 @@ function updateCost() {
         const amountInput = document.querySelector('input[name="amount"]'); // Modify selector if needed
         amountInput.value = cost;
         costDiv.textContent = `Trip Cost: kshs${cost}`;
+        // Set readonly attribute
+        amountInput.setAttribute('readonly', true);
     } else {
         costDiv.textContent = 'Please select pickup point and destination.';
+        // Remove readonly attribute if previously set
+        const amountInput = document.querySelector('input[name="amount"]'); // Modify selector if needed
+        amountInput.removeAttribute('readonly');
     }
 }
 
-/* Vehicle image */
 passengerDropdownContent.addEventListener('click', (event) => {
     if (event.target.tagName === 'A') {
         event.preventDefault();
@@ -102,6 +106,7 @@ passengerDropdownContent.addEventListener('click', (event) => {
         passengerDropbtn.textContent = `Select Number of passengers (${selectedPassengerCount})`;
         updateCost();
         const vehicleImage = document.getElementById('vehicle-image');
+        console.log(`Selected Passenger Count: ${selectedPassengerCount}`); // Add this line
         switch (selectedPassengerCount) {
             case '2000':
                 vehicleImage.src = '../images/saloon.jpg';
@@ -124,7 +129,8 @@ passengerDropdownContent.addEventListener('click', (event) => {
     }
 });
 
-/* Selected passenger output vehicle img */
+
+/* Selected passenger output */
 const vehicleTypes = {
     "2000": "Saloon",
     "3000": "Van",
@@ -182,11 +188,10 @@ function initPayPalButton() {
 
                     // Show a success message within this page, for example:
                     const element = document.getElementById("paypal-button-container");
-                    element.innerHTML = "<h3>Your payment has been received.<h3>";
+                    element.innerHTML = "";
                     element.innerHTML = "<h3>Thank you for your payment!</h3>";
 
-                    // Or go to another URL:
-                    actions.redirect('cheers.html');
+                    // Or go to another URL:  actions.redirect('thank_you.html');
                 });
             },
 
@@ -198,7 +203,6 @@ function initPayPalButton() {
 }
 initPayPalButton();
 
-
 /* Comments */
 function myFunction() {
     let data = ""; let name = document.getElementById("fullame").value
@@ -209,3 +213,96 @@ function myFunction() {
 
     document.getElementById("data").innerHTML = data  // display data to paragraph
 }
+
+/* mpesa */
+var button = document.getElementById("mpesaButton");
+
+if (button !== null) {
+    document.head.insertAdjacentHTML('beforeend', '<link rel=stylesheet href="https://cdn.jsdelivr.net/gh/muaad/mpesa_button@master/styles/style.css">');
+    img = '<img style="width: 35px; display: inline; margin: -8px;" src= "https://cdn.jsdelivr.net/gh/muaad/mpesa_button@master/images/mpesa.png">'
+    btnMarkup = '<a href="" id="mpesaBtn" class="mpesaButton">' + img + '<span style="margin-right: 15px;">Pay with Mpesa</span></a>'
+    phoneInstruction = '<strong><em>We will send an Mpesa payment request to this phone number</em></strong>'
+    form = '<form>\
+    <label for="amount" class="mpesaLabel">Amount</label><input class="mpesaInput" type="text" placeholder="2000" name="amount" id="mpesaAmount"></input><br>\
+    <label for="phone" class="mpesaLabel">Phone Number</label><input class="mpesaInput" type="text" placeholder="254722123456" name="phone" id="mpesaPhoneNumber"></input><br>' + phoneInstruction + '<br><br><button href="" id="mpesaSend" class="mpesaButton" style="width: 100%;">' + img + '<span style="margin-right: 15px;">Pay</span></button></form>'
+    formMarkup = '<div id="mpesaForm"><h3 class="mpesaHeader">Pay With Mpesa</h3><button id="closeMpesaForm" class="mpesaFormClose">&#x2716;</button>' + form + '</div>'
+    button.innerHTML = btnMarkup
+
+    success = '<div style="text-align: center;" class="animate-bottom">\
+    <h2>√ Success</h2>\
+    <p>An Mpesa payment request will be sent to your phone shortly</p>\
+  </div>'
+
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+        var formDiv = document.getElementById('mpesaForm');
+        // Check if the form exists before creating a new one
+        if (!formDiv) {
+            formDiv = document.createElement('div');
+            button.parentNode.insertBefore(formDiv, button.nextSibling);
+            formDiv.innerHTML = formMarkup;
+        }
+        formDiv.style.display = formDiv.style.display === 'block' ? 'none' : 'block';
+
+        // Close button functionality
+        var closeButton = document.getElementById("closeMpesaForm");
+        closeButton.addEventListener('click', function () {
+            formDiv.style.display = 'none';
+            // Toggle the visibility of the initial button
+            button.style.display = button.style.display === 'none' ? 'block' : 'none';
+        });
+
+        amountInput = document.getElementById("mpesaAmount")
+        phoneInput = document.getElementById("mpesaPhoneNumber")
+        phone = button.getAttribute('data-phone')
+        amount = button.getAttribute('data-amount')
+        url = button.getAttribute('data-url')
+        amountInput.value = amount
+        phoneInput.value = phone
+
+        payButton = document.getElementById("mpesaSend")
+        loaderDiv = document.createElement('div')
+        loaderDiv.setAttribute("id", "loader");
+        payButton.parentNode.insertBefore(loaderDiv, payButton.nextSibling);
+        loader = document.getElementById("loader")
+        loader.style.display = "none";
+        loader.style.margin = '-75px 0 0 -110px';
+
+        payButton.addEventListener('click', function (evt) {
+            evt.preventDefault();
+            payButton.disabled = true;
+            document.getElementById('mpesaPhoneNumber').disabled = true;
+            formDiv = document.getElementById('mpesaForm')
+            if (url !== undefined) {
+                var xhttp = new XMLHttpRequest();
+                xhttp.open("POST", url, true);
+                xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhttp.send('phone=' + phoneInput.value + '&amount=' + amountInput.value);
+                xhttp.onreadystatechange = function () {
+                    if (this.readyState == 4 && this.status == 200) {
+                        formDiv.innerHTML = success
+                    } else {
+                        formDiv.innerHTML = 'Something went wrong. Contact website developer. Error: "We could not POST to the URL specified!"'
+                    }
+                };
+            } else {
+                setTimeout(function () {
+                    formDiv.innerHTML = 'Something went wrong. Contact website developer. Error: "No URL specified!"'
+                }, 3000);
+            }
+            loader.style.display = "";
+        })
+    })
+}
+
+/** mpesa close button */
+button.addEventListener('click', function (e) {
+    // ... (existing code)
+    formDiv.style.display = formDiv.style.display === 'block' ? 'none' : 'block';
+
+    // Close button functionality
+    var closeButton = document.getElementById("closeMpesaForm");
+    closeButton.addEventListener('click', function () {
+        formDiv.style.display = 'none';
+    });
+});
